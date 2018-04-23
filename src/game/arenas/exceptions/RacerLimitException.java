2ad66a224fe61d9f0876a8b266b4e5ef0fb2933f
mreouven-study <37777368+mreouven-study@users.noreply.github.com>
2018-04-23 20:20:21 +0300

    ok

--- src/game/arenas/exceptions/RacerLimitException.java
+++ src/game/arenas/exceptions/RacerLimitException.java
@@ -4,6 +4,7 @@ package game.arenas.exceptions;
 public class RacerLimitException extends Exception{
 
 
+	
 	public RacerLimitException(String nmbre,String serial) {
 		super("Arena is full! ("+nmbre+" active racers exist). racer #"+serial+" was not added");
 	}
